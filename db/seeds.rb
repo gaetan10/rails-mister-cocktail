58d49083ea67f21313be70e86b2c6600be9dae07
Gaetan May <gaetan.may@gmail.com>
2017-08-10 17:28:24 +0100

    made it work (a bit)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Ingredient.destroy_all
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "rhum")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "whisky")
+
+
+# Cocktail.create(name: "cuba libre")
+# Cocktail.create(name: "gin tonic")
+# Cocktail.create(name: "mojito")
+# Cocktail.create(name: "sky on the rocks")
+# Cocktail.create(name: "margharita")
